dfb2ad41bfe64e591f5f07e9e8068a8aab9b8bcf
jpham9945 <jpham25@csu.fullerton.edu>
2021-10-07 06:08:57 -0700

    Class function ideas

--- app/src/main/java/com/example/myapplication/GroceryList.java
+++ app/src/main/java/com/example/myapplication/GroceryList.java
@@ -1,4 +1,13 @@
 package com.example.myapplication;
 
 public class GroceryList {
+
+    //Add items to the list in a vector
+
+    //Delete items from the list
+
+    //Save grocery list to open later
+
+    //Load in recipe ingredients to grocery list
+
 }
