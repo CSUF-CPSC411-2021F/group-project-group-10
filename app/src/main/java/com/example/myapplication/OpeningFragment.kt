e7434c47841dba0b8549e1ffc3a461f4682d7b8b
Drew Croteau <requin@csu.fullerton.edu>
2021-10-19 13:50:48 -0700

    Implemented onClickListener for the fragment transition between opening and login.

--- app/src/main/java/com/example/myapplication/OpeningFragment.kt
+++ app/src/main/java/com/example/myapplication/OpeningFragment.kt
@@ -5,6 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.navigation.findNavController
+import com.example.myapplication.databinding.FragmentOpeningBinding
 
 class OpeningFragment : Fragment() {
 
@@ -18,7 +21,20 @@ class OpeningFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+        // Creates a binding for the opening fragment.
+        val binding = FragmentOpeningBinding.inflate(layoutInflater)
+
+        // On click listener for the Pho bowl on the opening screen.
+        // Click it to proceed to the login fragment.
+        binding.phoBowl.setOnClickListener { view: View ->
+            view.findNavController()
+                .navigate(OpeningFragmentDirections
+                    .actionOpeningFragmentToLogin())
+
+        }
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_opening, container, false)
+        return binding.root
     }
 }
