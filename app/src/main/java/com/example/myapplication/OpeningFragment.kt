9651db7d0d2e60cc62015a81c523430f417081b5
Drew Croteau <requin@csu.fullerton.edu>
2021-10-19 00:15:52 -0700

    Created an opening fragment.

--- app/src/main/java/com/example/myapplication/OpeningFragment.kt
+++ app/src/main/java/com/example/myapplication/OpeningFragment.kt
@@ -6,7 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
-class login : Fragment() {
+class OpeningFragment : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,6 +19,6 @@ class login : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        return inflater.inflate(R.layout.fragment_opening, container, false)
     }
 }
