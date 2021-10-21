1bfdc9f02e6be52efc688b29a14debdf657428e5
cc506 <cchung28@ucmerced.edu>
2021-10-21 15:40:27 -0700

    finished ratings layout

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.RatingBar
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val rBar = findViewById<RatingBar>(R.id.ratings)
+        val comment = findViewById<EditText>(R.id.comment)
+
+        if (rBar != null) {
+            val button = findViewById<Button>(R.id.button)
+            button?.setOnClickListener {
+                val msg = rBar.rating.toString()
+                Toast.makeText(this@MainActivity,
+                    "Rating is: $msg & ${comment.text}", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
