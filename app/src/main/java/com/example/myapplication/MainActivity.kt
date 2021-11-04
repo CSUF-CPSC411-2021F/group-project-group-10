9f60f4574686f3a5fb437ad0f5b5a86637787d07
cc506 <cchung28@ucmerced.edu>
2021-11-04 16:00:32 -0700

    implementing recipie page and its reviews. still have errors

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,27 +1,31 @@
 package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.RatingBar
 import android.widget.Toast
+import com.example.myapplication.databinding.ActivityMainBinding
+import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val rBar = findViewById<RatingBar>(R.id.ratings)
-        val comment = findViewById<EditText>(R.id.comment)
+        val review = mutableListOf<Text>()
 
-        if (rBar != null) {
-            val button = findViewById<Button>(R.id.button)
-            button?.setOnClickListener {
-                val msg = rBar.rating.toString()
-                Toast.makeText(this@MainActivity,
-                    "Rating is: $msg & ${comment.text}", Toast.LENGTH_SHORT).show()
-            }
-        }
+        var binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        var adapter = ReviewAdapter(this, review, object : ReviewAdapter.OnClickListener {
+
+            val i = Intent(this@MainActivity, AddReviewActivity::class.java)
+
+            )
+        })
+
     }
 }
