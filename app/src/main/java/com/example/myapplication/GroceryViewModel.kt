e370d32428079e1a93d1d95902d8125ef6167df2
jpham9945 <jpham25@csu.fullerton.edu>
2021-12-06 18:31:03 -0800

    December 6, 2021 Updates

---
+++ app/src/main/java/com/example/myapplication/GroceryViewModel.kt
@@ -0,0 +1,58 @@
+package com.example.myapplication
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
+import com.example.myapplication.database.Grocery
+import com.example.myapplication.database.GroceryDao
+import kotlinx.coroutines.launch
+
+
+class GroceryViewModel(
+    val database: GroceryDao, // Data access object for the Grocery entity
+    application: Application
+) : AndroidViewModel(application) {
+
+    // Used to assign and retrieve name and location values from the interface.
+    var name = MutableLiveData("")
+//    var information = MutableLiveData("")
+
+    val groceryList = database.getAllGroceries()
+
+    /**
+     * Inserts the Grocery object into the database.
+     */
+    fun insert() {
+        // Launch coroutines in the viewModelScope so that the coroutines are automatically
+        // canceled when the ViewModel is destroyed.
+        viewModelScope.launch {
+            // Create Grocery object using data stored in the EditText views
+            var grocery = Grocery()
+            grocery.name = name.value.toString()
+//            grocery.information = information.value.toString()
+            // Insert data to the database using the insert coroutine.
+            database.insert(grocery)
+        }
+    }
+
+    /**
+     * Deletes all Grocery entities in the database.
+     */
+    fun clear() {
+        // Launch coroutines in the viewModelScope so that the coroutines are automatically
+        // canceled when the ViewModel is destroyed.
+        viewModelScope.launch {
+            // Delete data from the database using the clear coroutine.
+            database.clear()
+        }
+    }
+
+    fun delete(grocery: Grocery) {
+        viewModelScope.launch {
+            // Delete data from the database using the clear coroutine.
+            database.deleteGrocery(grocery.groceryId)
+        }
+    }
+
+}
--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.example.myapplication
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import com.example.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        // Create data binding and assign layout for the activity.
+        val binding: ActivityMainBinding? =
+            DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        // Setup navigation controller and action bar.
+        val navController = this.findNavController(R.id.nav_host)
+        NavigationUI.setupActionBarWithNavController(this, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = this.findNavController(R.id.nav_host)
+        return navController.navigateUp()
     }
 }
